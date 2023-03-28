1f71b8b143b0fb3d773797898e850d4d9c94804a
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-03-28 10:46:15 -0300

    Verificação de usuário logado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthContext } from "./contexts/AuthContext";
+import { auth } from "./firebase/config";
 import { Cadastro } from "./pages/Cadastro/Cadastro";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
@@ -8,17 +11,32 @@ import { Root } from "./pages/Root/Root";
 
 
 export function App() {
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
+  useEffect(() => {
+    // Monitorar/detectar usuário conectado
+    // Fica sabendo quando loga/desloga
+    onAuthStateChanged(auth, (user) => {
+      // user === nulo = deslogado
+      // user tem objeto = logado
+      setUsuarioLogado(user);
+    })
+    // Esse efeito vai rodar apenas um vez
+    // Quando o app for renderizado//inicializado
+  }, [])
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Root />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthContext.Provider value={usuarioLogado}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Root />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
       <Toaster />
     </>
   )
--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
 import { Menu } from "../../components/Menu/Menu";
+import { AuthContext } from "../../contexts/AuthContext";
 
 // Layout principal do App com Navbar fixa
 // As páginas com Navbar fixa: home, livros, expréstimos, etc
 export function Root() {
+  const usuarioLogado = useContext(AuthContext)
+  if (usuarioLogado === null) {
+    return <Navigate to="/login" />
+  }
   return (
     <>
       <header>
