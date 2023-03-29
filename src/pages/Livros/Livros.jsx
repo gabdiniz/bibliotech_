8a85f417c587f71f417eef7290ea12e5f4742cd9
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-03-29 12:11:46 -0300

    Implementação deletar livro

--- src/pages/Livros/Livros.jsx
+++ src/pages/Livros/Livros.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
-import { getLivros } from "../../firebase/livros";
+import { deleteLivro, getLivros } from "../../firebase/livros";
 import "./Livros.css"
 
 export function Livros() {
@@ -10,10 +11,24 @@ export function Livros() {
   const [livros, setLivros] = useState(null);
 
   useEffect(() => {
+    initializeTable();
+  }, []);
+
+  function initializeTable() {
     getLivros().then((busca) => {
       setLivros(busca)
     })
-  }, []);
+  }
+
+  function onDeleteLivro(id, titulo) {
+    const deletar = window.confirm(`Tem certeza que deseja deletar ${titulo}?`);
+    if (deletar) {
+      deleteLivro(id).then(() => {
+        toast.success(`${titulo} deletado.`)
+        initializeTable();
+      })
+    }
+  }
 
   return (
     <div className="livros">
@@ -47,7 +62,7 @@ export function Livros() {
                       <td><img src={livro.urlCapa} alt={`Capa do livro: ${livro.titulo}`} /></td>
                       <td className="d-flex flex-column" >
                         <Button variant="warning" size="sm" as={Link} to={`/livros/editar/${livro.id}`}> <i className="bi bi-pencil-square"></i></Button>
-                        <Button variant="danger" size="sm" className="mt-2 px-1"> <i className="bi bi-trash3"></i></Button>
+                        <Button variant="danger" size="sm" className="mt-2 px-1"> <i className="bi bi-trash3" onClick={() => { onDeleteLivro(livro.id, livro.titulo) }}></i></Button>
                       </td>
                     </tr>
                   )
